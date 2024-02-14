ed3e0b688fadddf7c0582fe0038cfa45974114bc
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-02-14 22:45:28 +0530

    Update 1030. Matrix Cells in Distance Order.java

--- 1030. Matrix Cells in Distance Order.java
+++ 1030. Matrix Cells in Distance Order.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
         int[][] result = new int[R * C][];
-        result[0] = new int[] {r0, c0};            // Add start point r0,c0 which has distance 0.
+        result[0] = new int[] {r0, c0};            
         int resultIdx = 1;
         int lim = Math.max( r0, R-r0-1 ) + Math.max( c0, C-c0-1 );
-		                                           // lim is highest distance value.
+		                                          
         for (int dist = 1; dist <= lim; dist++) {  // Process 'lim' diamond-shapes having distance 'dist'.
             int r = r0 - dist;
             int c = c0;
